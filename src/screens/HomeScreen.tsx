ccb78ab5b1f1aa61f20200a5d29e4e4e688a2eca
RDOToole89 <roibinotoole@gmail.com>
2022-03-09 16:33:33 +0100

    add infobar with play button to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, ImageBackground, ScrollView } from 'react-native';
+import { Button, IconButton } from 'react-native-paper';
 import { GLOBAL } from '../global/styles/global';
+import { Ionicons } from '@expo/vector-icons';
 import { TYPOGRAPHY } from '../global/styles/typography';
 import image from '../../assets/images/posters/stranger-things.jpg';
 import { TagMapper } from '../components/TagMapper';
@@ -26,6 +28,44 @@ const HomeScreen = () => {
           tagColor='#fff'
           symbolColor='gold'
         />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          paddingVertical: GLOBAL.SPACING.sm,
+          backgroundColor: TYPOGRAPHY.COLOR.BlackSecondary,
+        }}>
+        <View
+          style={{
+            alignItems: 'center',
+          }}>
+          <Ionicons name='add' size={30} color={TYPOGRAPHY.COLOR.White} />
+          <Text style={{ fontSize: TYPOGRAPHY.FONT_SIZES.sm, color: '#fff' }}>
+            My List
+          </Text>
+        </View>
+        <Button
+          style={{ backgroundColor: '#fff' }}
+          icon='play'
+          color='#000'
+          onPress={() => console.log('Pressed')}>
+          Play
+        </Button>
+        <View
+          style={{
+            alignItems: 'center',
+          }}>
+          <Ionicons
+            name='information-circle-outline'
+            size={30}
+            color={TYPOGRAPHY.COLOR.White}
+          />
+          <Text style={{ fontSize: TYPOGRAPHY.FONT_SIZES.sm, color: '#fff' }}>
+            Info
+          </Text>
+        </View>
       </View>
     </View>
   );
