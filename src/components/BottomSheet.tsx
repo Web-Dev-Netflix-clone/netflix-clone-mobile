c97c114d8dbfaad88e2c42e967f31587345c1005
Fatos Varol <ftsvarol@gmail.com>
2022-03-16 09:48:05 +0100

    make first version of bottomsheet

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,17 +1,55 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { StyleSheet, Button, Dimensions } from "react-native";
+import React, { useCallback, useEffect } from "react";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
-const BottomSheetRenderContent = () => {
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+export const MAX_TRANSLATE_Y = -SCREEN_HEIGHT / 3;
+
+const BottomSheet = () => {
+  const translateY = useSharedValue(0);
+  const active = useSharedValue(false);
+
+  const toggleBottomSheet = useCallback((destination: number) => {
+    "worklet";
+    active.value = destination !== 0;
+
+    translateY.value = withTiming(destination);
+  }, []);
+
+  const isActive = useCallback(() => {
+    return active.value;
+  }, []);
+
+  useEffect(() => {
+    toggleBottomSheet(MAX_TRANSLATE_Y);
+  }, []);
+
+  const rBottomSheetStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: translateY.value }],
+    };
+  });
+
   return (
-    <View
-      style={{
-        backgroundColor: 'white',
-        padding: 16,
-        height: 450,
-      }}>
-      <Text>Swipe down to close</Text>
-    </View>
+    <Animated.View style={[styles.bottomSheetContainer, rBottomSheetStyle]}>
+      <Button title="Close" onPress={() => toggleBottomSheet(0)} />
+    </Animated.View>
   );
 };
 
-export default BottomSheetRenderContent;
+export default BottomSheet;
+
+const styles = StyleSheet.create({
+  bottomSheetContainer: {
+    height: SCREEN_HEIGHT,
+    width: "100%",
+    backgroundColor: "white",
+    position: "absolute",
+    top: SCREEN_HEIGHT,
+    borderRadius: 20,
+  },
+});
