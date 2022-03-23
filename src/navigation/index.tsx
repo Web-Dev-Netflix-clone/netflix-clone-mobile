2f5c027ae3c8757f1568679dac93e8a6dff29ed1
Fatos Varol <ftsvarol@gmail.com>
2022-03-23 16:05:00 +0100

    move bottomsheet into a seperate component

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -12,29 +12,16 @@ import { DrawerTabNavigator } from "./Drawer";
 import { LoginStack } from "./AuthStack";
 import { useSelector } from "../hooks/useTypedSelector";
 import BottomSheet from "../components/BottomSheet";
-import Modal from "react-native-modal";
-import { useActions } from "../hooks/useActions";
 
 const navTheme = DefaultTheme;
 navTheme.colors.background = "transparent";
 export const Navigation = () => {
-  const { showBottomSheet } = useActions();
   const isSignedIn = useSelector((state) => state.userData.isLoggedIn);
-  const bottomSheetVisible = useSelector(
-    (state) => state.appState.showBottomSheet
-  );
 
   return (
     <NavigationContainer theme={navTheme}>
       {isSignedIn ? <LoginStack /> : <DrawerTabNavigator />}
-      <Modal
-        coverScreen={true}
-        hasBackdrop={false}
-        // style={styles.view}
-        isVisible={bottomSheetVisible}
-      >
-        <BottomSheet />
-      </Modal>
+      <BottomSheet />
       {/* <RootNavigator /> */}
     </NavigationContainer>
   );
