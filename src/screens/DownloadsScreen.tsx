359cd3fb33f61d54d04c3c4d69e2c3103523b5a1
RDOToole89 <roibinotoole@gmail.com>
2022-03-30 13:04:24 +0200

    add v1 of downloads page

--- src/screens/DownloadsScreen.tsx
+++ src/screens/DownloadsScreen.tsx
@@ -1,16 +1,84 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { GLOBAL } from '../global/styles/global';
+import { TYPOGRAPHY } from '../global/styles/typography';
+import { Button } from 'react-native-paper';
+import banner from '../../assets/netflix-banner/download-screen-resized2.png';
 
 const DownloadsScreen = () => {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-      }}>
-      <Text>Downloads Screen</Text>
+      style={[
+        GLOBAL.LAYOUT.SafeArea,
+        {
+          flex: 1,
+          backgroundColor: '#000',
+          paddingTop: 100,
+          padding: GLOBAL.SPACING.md,
+          // justifyContent: 'space-between',
+        },
+      ]}>
+      <View
+        style={[
+          GLOBAL.LAYOUT.rowCenter,
+          { paddingBottom: GLOBAL.SPACING.xxxl },
+        ]}>
+        <FontAwesome name='cog' size={30} color={TYPOGRAPHY.COLOR.GreyLight} />
+        <Text
+          style={[
+            TYPOGRAPHY.FONT.subtitle,
+            { marginLeft: 15, marginBottom: 0 },
+          ]}>
+          Smart Downloads
+        </Text>
+      </View>
+      <View>
+        <Text style={TYPOGRAPHY.FONT.h2}>Introducing Downloads for You</Text>
+        <Text style={TYPOGRAPHY.FONT.subtitle}>
+          We'll download a personalized selection of movies and shows for you,
+          so there's always something to watch on your phone.
+        </Text>
+      </View>
+
+      <ImageBackground
+        source={banner}
+        resizeMode='contain'
+        style={{
+          height: 300,
+
+          marginBottom: GLOBAL.SPACING.xxl,
+        }}
+      />
+      <View style={{ alignItems: 'center' }}>
+        <Button
+          style={{
+            backgroundColor: TYPOGRAPHY.COLOR.Blue,
+            width: '100%',
+            marginBottom: GLOBAL.SPACING.xxxxl,
+          }}
+          uppercase={false}
+          color='#fff'
+          onPress={() => console.log('Pressed')}>
+          Set Up
+        </Button>
+        <Button
+          style={{
+            backgroundColor: TYPOGRAPHY.COLOR.DarkSecondary,
+            width: '60%',
+          }}
+          uppercase={false}
+          color='#fff'
+          onPress={() => console.log('Pressed')}>
+          <Text
+            style={{
+              fontSize: TYPOGRAPHY.FONT_SIZES.sm,
+              fontFamily: TYPOGRAPHY.FONT.PrimaryBold,
+            }}>
+            Find More to Download
+          </Text>
+        </Button>
+      </View>
     </View>
   );
 };
